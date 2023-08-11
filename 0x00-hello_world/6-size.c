e78c164410dbb384d3ecaca9bbe050b7b3deeb18
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-08-10 17:06:03 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(z));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(j));
+	return(0);
 }
