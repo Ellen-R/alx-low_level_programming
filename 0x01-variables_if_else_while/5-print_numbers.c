cb5f83fbe9068995e856f26ccd10dde1047d9f51
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-08-11 19:06:28 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of progam
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int number;
+
+	for (mumber = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+
+	return (0);
+}
