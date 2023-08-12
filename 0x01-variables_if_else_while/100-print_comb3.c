20251d1ae0637451ec897f38d902281a655704bb
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-08-11 20:18:02 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlin.h>
+
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	k = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		k++;
+	}
+	putchar('\n');
+
+	return (0);
+}
