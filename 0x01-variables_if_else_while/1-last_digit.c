b666a1342c7e459159716f802fd8b17a94332f83
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-08-11 17:52:05 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,26 +5,27 @@
  * main - Entry point
  * Return: 0 (success)
  */
-
 int main(void)
 {
-        int n;
+	int n;
 	int last_digit;
 
-        srand(time(NULL));
+	srand(time(NULL));
 
-        n = rand() - RAND_MAX / 2;
-	last_digit = n % 10 /* anytime you divide by 10, you will get last digit of your dividend*/
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-        if (last_digit > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-        }
-        else if (last_digit == 0)
-        {
-                printf("Last digit of %d is %d and is 0\n", n, last_digit);
-        }
-        else if (last_digit <6 && last_digit != 0)
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-        return (0);
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else if (last_digit < 6 && last_digit != 0)
+	{
+		printf("Last digit of %d and %d is less than 6 and not 0\n", n, last_digit);
+	}
+	return (0);
 }
