caacc5426d2819c951c6c6d4195adec402a19c80
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-08-21 11:59:54 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 -  takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: check the character
+ * Return: n on success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
