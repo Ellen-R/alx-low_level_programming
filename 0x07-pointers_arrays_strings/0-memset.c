dba4dc24201cc04699b0c4111288856914eaad49
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-08-28 08:07:55 -0700

    multi dimensional arrays and pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,24 @@
 #include "main.h"
+
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
+ * _memset - Fills memory with a constant byte.
+ * @s: Pointer to the memory area to be filled.
+ * @b: The constant byte to fill the memory with.
+ * @n: The number of bytes to fill.
  *
- * Return: changed array with new value for n bytes
+ * Return: A pointer to the memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	char *original_s = s; /* Save the original pointer */
 
-	for (; n > 0; i++)
+	while (n > 0)
 	{
-		s[i] = b;
-		n--;
+		*s = b; /* Set the value of b at the current memory location */
+		s++;   /* Move to the next memory location */
+		n--;   /* Decrement the remaining count of bytes */
 	}
-	return (s);
+
+	return (original_s); /* Return the original pointer */
 }
 
