8e52247f59d1e8bed57f2502f9f6a89fdd27975c
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-09-07 11:39:48 -0700

    mallocs

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,6 +12,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -36,7 +37,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 		new_size = old_size;
 
-	for (unsigned int i = 0; i < new_size; i++)
+	for (i = 0; i < new_size; i++)
 		((char *)new_ptr)[i] = ((char *)ptr)[i];
 
 	free(ptr);
