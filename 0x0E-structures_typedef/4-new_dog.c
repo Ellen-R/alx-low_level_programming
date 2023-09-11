d378540071fef64ee3e37d7f67e9d9b56df8e9b7
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-09-11 14:46:24 -0700

    structs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,11 +5,11 @@
 /**
  * _strdup - Duplicates a string
  * @str: The string to duplicate
- *
+ *@dup_str: pointer to the buffer in which we copy the string
  * Return: A pointer to the newly allocated duplicated string.
  *         NULL if memory allocation fails or str is NULL.
  */
-char *_strdup(char *str)
+char *_strdup(char *str, char *dup_str)
 {
 	char *dup_str;
 	int i, len;
@@ -22,7 +22,7 @@ char *_strdup(char *str)
 	while (str[len] != '\0')
 		len++;
 
-	dup_str = malloc((len + 1) * sizeof(char));
+	/*dup_str = malloc((len + 1) * sizeof(char));*/
 
 	/*if (dup_str == NULL)*/
 		/*return (NULL);*/
