b99c9184b8174247078d382dc430938a60067aa0
Mamphosi Ellen Ralepoma <mamaphosiellenr@gmail.com>
2023-09-11 14:39:26 -0700

    structs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,20 +16,21 @@ char *_strdup(char *str)
 
 	len = 0;
 
-	if (str == NULL)
-		return (NULL);
+	/*if (str == NULL)*/
+		/*return (NULL);*/
 
-	while (str[len])
+	while (str[len] != '\0')
 		len++;
 
-	 dup_str = malloc((len + 1) * sizeof(char));
+	/* dup_str = malloc((len + 1) * sizeof(char));*/
 
-	if (dup_str == NULL)
-		return (NULL);
+	/*if (dup_str == NULL)*/
+		/*return (NULL);*/
 
-	for (i = 0; i <= len; i++)
+	for (i = 0; i < len; i++)
 		dup_str[i] = str[i];
-
+	
+	dup_str[i] = '\0';
 	return (dup_str);
 }
 
